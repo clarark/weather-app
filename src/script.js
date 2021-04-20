c13f4904123eca51f21b3a77d76bb8869562f895
clarark <clara.rk@hotmail.com>
2021-04-20 16:55:16 +0200

    improved the unit links

--- src/script.js
+++ src/script.js
@@ -74,15 +74,19 @@ function formatDate(now) {
 
     function displayFahrenheitTemperature(event) {
       event.preventDefault();
-      let a = document.querySelector("#temperature");
+      let temperatureElement = document.querySelector("#temperature");
+      celsiusLink.classList.remove("active");
+      fahrenheitLink.classList.add("active");
       let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32
-      a.innerHTML = Math.round(fahrenheitTemperature);
+      temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
       
     }
 
     function displayCelsiusTemperature(event) {
       event.preventDefault();
       let temperatureElement = document.querySelector("#temperature");
+      celsiusLink.classList.add("active");
+      fahrenheitLink.classList.remove("active");
       temperatureElement. innerHTML = Math.round(celsiusTemperature);
 
     }
