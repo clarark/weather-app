29527506084114e7febe69c277e930c7bd2d17f3
clarark <clara.rk@hotmail.com>
2021-04-27 19:53:39 +0200

    added forecast JS

--- src/script.js
+++ src/script.js
@@ -15,6 +15,35 @@ function formatDate(now) {
     return `${day} ${hour}:${minutes}`;
   
   } 
+
+  function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class="row">`;
+    let days = ["Thu", "Fri", "Sat"];
+    days.forEach(function(day) {
+      forecastHTML = forecastHTML +
+      `
+       <div class="col-2">
+           <div class="weather-forecast-date">
+               ${day}
+           </div>
+           <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="" width="42"/>
+           <div class="weather-forecast-temperatures">
+               <span class="weather-forecast-temperature-max">
+                   18°
+               </span>
+               <span class="weather-forecast-temperature-min">
+                   12°
+               </span>
+           </div>
+       </div>
+      `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
   
   
   function showTemperature(response) {
@@ -111,4 +140,6 @@ function formatDate(now) {
   celsiusLink.addEventListener("click", displayCelsiusTemperature);
   
   changeCity("New York");
+
+  displayForecast();
   
