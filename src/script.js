7f98a8bdc629d9bade89a73da06f7bcdae52fbe0
clarark <clara.rk@hotmail.com>
2021-04-27 21:41:52 +0200

    removed the fahrenheit link

--- src/script.js
+++ src/script.js
@@ -122,25 +122,7 @@ function getForecast(coordinates) {
       axios.get(apiUrl).then(showTemperature);
     }
 
-    function displayFahrenheitTemperature(event) {
-      event.preventDefault();
-      let temperatureElement = document.querySelector("#temperature");
-      celsiusLink.classList.remove("active");
-      fahrenheitLink.classList.add("active");
-      let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-      temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-      
-    }
-
-    function displayCelsiusTemperature(event) {
-      event.preventDefault();
-      let temperatureElement = document.querySelector("#temperature");
-      celsiusLink.classList.add("active");
-      fahrenheitLink.classList.remove("active");
-      temperatureElement. innerHTML = Math.round(celsiusTemperature);
-
-    }
-
+    
 
   let celsiusTemperature = null;
 
@@ -153,12 +135,6 @@ function getForecast(coordinates) {
   let date = document.querySelector(".cheese");
   let now = new Date();
   date.innerHTML = formatDate(now);
-
-  let fahrenheitLink = document.querySelector("#fahrenheit-link");
-  fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-  let celsiusLink = document.querySelector("#celsius-link");
-  celsiusLink.addEventListener("click", displayCelsiusTemperature);
   
   changeCity("New York");
 
